e2c6179caf5c73cd48c73b1e4fa66403b8d6fead
Michael Alexander <alexandermw@gmail.com>
2014-07-21 16:46:53 -0700

    Update bubble_sort.rb
    merged in bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -19,4 +19,23 @@ def bubble_sort (array)
   array
 end
 
+def bubble_sort_by (array)
+  sorted = false
+  until sorted do
+    sorted=true
+    (0..array.size-2). each do |x|
+      sorted = false if yield(array[x], array[x+1]) < 0
+    end
+    (0..array.size-2).each do |y|
+      if yield(array[y],array[y+1]) < 0 
+        array[y+1], array[y] = array[y], array[y+1]
+      end
+    end
+  end
+  array
+end
+
 print bubble_sort([4,3,78,2,0,2])
+puts "\n----------"
+print bubble_sort_by(["hi","hello","hey"]) {|left,right| right.length - left.length}
+
