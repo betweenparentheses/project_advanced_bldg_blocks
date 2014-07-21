a735c0770c47710a68447093e1b9f86c48f2c1dd
Michael Alexander <alexandermw@gmail.com>
2014-07-21 15:43:14 -0700

    Create bubble_sort_by.rb
    script still needs work.

---
+++ bubble_sort_by.rb
@@ -0,0 +1,20 @@
+#still working on this
+
+def bubble_sort_by (array)
+  sorted = false
+  until sorted do
+    sorted=true
+    (0..array.size-2). each do |x|
+      sorted = false if yield(array[x], array[x+1])
+    end
+    (0..array.size-2).each do |y|
+      if yield(array[y],array[y+1]) < 0 
+        array[y+1], array[y] = array[y], array[y+1]
+      end
+    end
+  end
+  array
+end
+
+print bubble_sort_by(["hi","hello","hey"]) {|left,right| right.length - left.length}
+
