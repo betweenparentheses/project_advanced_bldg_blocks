d342d01ee64cb73722318ace7e09cbbe8d9251a2
Michael Alexander <alexandermw@gmail.com>
2014-07-21 15:44:33 -0700

    fixed infinite loop in line 8

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -5,7 +5,7 @@ def bubble_sort_by (array)
   until sorted do
     sorted=true
     (0..array.size-2). each do |x|
-      sorted = false if yield(array[x], array[x+1])
+      sorted = false if yield(array[x], array[x+1]) < 0
     end
     (0..array.size-2).each do |y|
       if yield(array[y],array[y+1]) < 0 
